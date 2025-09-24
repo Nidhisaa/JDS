b2d3849dc3acd6f0403bd16573870813b0252e29
Nidhisaa <senthilnidhi57@gmail.com>
2025-09-24 14:36:45 +0530

    palindrome updated

--- JavaBasics/palindrome.java
+++ JavaBasics/palindrome.java
@@ -1,5 +1,6 @@
-import java.util.*;
-public class Palindrome { // Class name capitalized to match Java conventions
+package JavaBasics;
+import java.util.Scanner;
+public class palindrome {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number: ");
@@ -23,6 +24,3 @@ public class Palindrome { // Class name capitalized to match Java conventions
         sc.close();
     }
 }
-
-        
-    
